3c164822d5a01e2f2acde1370a8142f8722be11c
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2024-04-22 13:35:58 -0400

    init

---
+++ README.md
@@ -0,0 +1,74 @@
+# css-follow-cursor
+
+_Because interactive eye-candy is amazing_
+
+Ever wanted to create various effects in CSS which which react to mouse movement? Well, with the power of this small library and dynamic CSS variables, you can!
+
+## Usage
+
+See the elements with the `test-area` class in `tests/test.html` file for a simple example.
+
+All you have to do is add the `follow-cursor-vars` class to any element you wish. The element will then have the
+following CSS variables:
+* `--cursor-pos-x-px`: The cursor position on the X axis, where `0px` is on the left side of the element +
+  padding + border.
+* `--cursor-pos-y-px`: The cursor position on the Y axis, where `0px` is on the top side of the element +
+  padding + border.
+* `--cursor-pos-x-percentage`: The cursor position on the X axis, where `0%` is on the left side of the element +
+  padding + border, and `100%` is on the right side of the element + padding + border. 
+* `--cursor-pos-y-percentage`: The cursor position on the Y axis, where `0%` is on the top side of the element +
+  padding + border, and `100%` is on the bottom side of the element + padding + border.
+* `--cursor-pos-x-fraction`: Same as `--cursor-pos-x-percentage`, except `0` to `1` instead of `0%` to `100%`
+  respectively. Useful with CSS's `calc()`.
+* `--cursor-pos-y-fraction`: Same as `--cursor-pos-y-percentage`, except `0` to `1` instead of `0%` to `100%`
+  respectively. Useful with CSS's `calc()`.
+* `--cursor-pos-fade-percentage`: A value between `0%` and `100%`, with the value approaching `100%` while the cursor
+  hovers over the element, and `0%` while the cursor is not hovering over the element.
+* `--cursor-pos-fade-fraction`: A value between `0` and `1`, with the value approaching `1` while the cursor hovers
+  over the element, and `0` while the cursor is not hovering over the element. Useful with CSS's `calc()`.
+
+This library also exports the following function:
+* `addFollowCursorClass(...classes: string[])`: Allows you to add your own class so you're not forced to use
+  `follow-cursor-vars`.
+
+The values of the `--cursor-pos-fade-percentage` and `--cursor-pos-fade-fraction` variables can be influenced using the
+variables listed below:
+* `--cursor-fade-in-time`: A value in either seconds (`s`) or milliseconds (`ms`) specifying how long it will take the
+  the `--cursor-pos-fade-*` variables to go from their minimum value (`0%` or `0`) to their maximum value (`100%` or
+  `1`) after the cursor enters the element. Defaults to `0s`.
+* `--cursor-fade-out-time`: A value in either seconds (`s`) or milliseconds (`ms`) specifying how long it will take the
+  the `--cursor-pos-fade-*` variables to go from their maximum value (`100%` or `1`) to their minimum value (`0%` or
+  `0`) after the cursor leaves the element. Defaults to `0s`.
+* `--cursor-fade-function`: This can be set to a
+  [CSS easing function](https://developer.mozilla.org/en-US/docs/Web/CSS/easing-function). This function will be used
+  for the "fade in" timing function. The "fade out" timing function will be the reverse of the "fade in". For example,
+  `ease-in` will effectivly become `ease-out`. This design choice was made in order to make partial fade ins/outs consistent with full fade ins/outs.
+
+If the `follow-cursor-vars` class (or the class you specified when using the `addFollowCursorClass` function) exists
+on the root `<html>` element, (or the root `<svg>` element) then these CSS variables will also be available.
+
+* `--cursor-window-pos-x-px`: The cursor position on the X axis, where `0px` is the left side of the viewport, and
+  increases rightwards.
+* `--cursor-window-pos-y-px`: The cursor position on the Y axis, where `0px` is the top of the viewport and increases
+  downwards.
+* `--cursor-window-pos-x-percentage`: The cursor position on the X axis, where `0%` is on the left side of the viewport
+  and `100%` is on the right side of the viewport. 
+* `--cursor-window-pos-y-percentage`: The cursor position on the Y axis, where `0%` is on the top side of the viewport
+  and `100%` is on the bottom side of the viewport.
+* `--cursor-window-pos-x-fraction`: Same as `--cursor-window-pos-x-percentage`, except `0` to `1` instead of `0%` to
+  `100%` respectively. Useful with CSS's `calc()`.
+* `--cursor-window-pos-y-fraction`: Same as `--cursor-window-pos-y-percentage`, except `0` to `1` instead of `0%` to `100%`
+  respectively. Useful with CSS's `calc()`.
+* `--cursor-window-pos-fade-percentage`: A value between `0%` and `100%`, with the value approaching `100%` while the cursor
+  is in the viewport, and `0%` while the cursor is not in the viewport.
+* `--cursor-window-pos-fade-fraction`: A value between `0` and `1`, with the value approaching `1` while the cursor is
+  in the viewport, and `0` while the cursor is not in the viewport.
+
+## Installation
+
+You can add the `css-follow-cursor` package as a dependency of your npm project. Just be sure to import it 
+somewhere so that the appropriate events are listened to.
+
+If you run `npm install && npm run build-standalone` after cloning this repository, you will have a self-contained
+version of this library bundled with the external functions depended upon that you can use in a normal `<script>` tag.
+(Woah, old school!)
---
+++ src/index.ts
@@ -0,0 +1,243 @@
+import { parseCSSTime, parseCSSTimingFunction } from "@aritz-cracker/browser-utils";
+
+const hoverClasses = new Set(["follow-cursor-vars"]);
+
+let curTime = 0;
+let mouseX = 0;
+let mouseY = 0;
+type HoverData = {
+	fadeStartTime: number,
+	fadeTime: number,
+	timingFunction: (t: number) => number
+	fadingOut: boolean
+};
+
+const hoverDataMap: Map<HTMLElement | SVGElement, HoverData> = new Map();
+const listenedElements: Set<HTMLElement | SVGElement> = new Set();
+
+function startListeningToElement(elem: HTMLElement | SVGElement) {
+	elem.addEventListener("mouseenter", mouseEnterCallback, {passive: true});
+	elem.addEventListener("mouseleave", mouseLeaveCallback, {passive: true});
+	listenedElements.add(elem);
+	if (elem == document.documentElement) {
+		elem.style.setProperty("--cursor-window-pos-fade-fraction", "0");
+		elem.style.setProperty("--cursor-window-pos-fade-percentage", "0%");
+	} else {
+		elem.style.setProperty("--cursor-pos-fade-fraction", "0");
+		elem.style.setProperty("--cursor-pos-fade-percentage", "0%");
+	}
+	upateMousePosVars(elem)
+	if (elem.matches(":hover")) {
+		startFadeIn(elem);
+	}
+}
+function stopListeningToElement(elem: HTMLElement | SVGElement) {
+	elem.removeEventListener("mouseenter", mouseEnterCallback);
+	elem.removeEventListener("mouseleave", mouseLeaveCallback);
+	if (elem.matches(":hover")) {
+		startFadeOut(elem);
+	}
+	listenedElements.delete(elem);
+}
+function startFadeIn(elem: HTMLElement | SVGElement) {
+	const elemStyle = getComputedStyle(elem);
+	let fadeInTime = parseCSSTime(elemStyle.getPropertyValue("--cursor-fade-in-time"));
+	if (isNaN(fadeInTime) || fadeInTime < 0) {
+		fadeInTime = 0;
+	}
+	const hoverData = hoverDataMap.get(elem);
+	if (hoverData) {
+		let timeCompleted = (curTime - hoverData.fadeStartTime) / hoverData.fadeTime;
+		if (timeCompleted > 1) {
+			timeCompleted = 1;
+		}
+		hoverData.fadeStartTime = curTime - fadeInTime * (1 - timeCompleted);
+		hoverData.fadeTime = fadeInTime;
+		hoverData.fadingOut = false;
+	} else {
+		hoverDataMap.set(elem, {
+			fadeStartTime: curTime,
+			fadeTime: fadeInTime,
+			timingFunction: parseCSSTimingFunction(
+				elemStyle.getPropertyValue("--cursor-fade-function")
+			) ?? parseCSSTimingFunction("linear")!,
+			fadingOut: false
+		});
+	}
+}
+function startFadeOut(elem: HTMLElement | SVGElement) {
+	const elemStyle = getComputedStyle(elem);
+	let fadeOutTime = parseCSSTime(elemStyle.getPropertyValue("--cursor-fade-out-time"));
+	if (isNaN(fadeOutTime) || fadeOutTime < 0) {
+		fadeOutTime = 0;
+	}
+	const hoverData = hoverDataMap.get(elem);
+	if (hoverData) {
+		let timeCompleted = (curTime - hoverData.fadeStartTime) / hoverData.fadeTime;
+		if (timeCompleted > 1) {
+			timeCompleted = 1;
+		}
+		hoverData.fadeStartTime = curTime - fadeOutTime * (1 - timeCompleted);
+		hoverData.fadeTime = fadeOutTime;
+		hoverData.fadingOut = true;
+	} else {
+		hoverDataMap.set(elem, {
+			fadeStartTime: curTime,
+			fadeTime: fadeOutTime,
+			timingFunction: parseCSSTimingFunction(
+				elemStyle.getPropertyValue("--cursor-fade-function")
+			) ?? parseCSSTimingFunction("linear")!,
+			fadingOut: true
+		});
+	}
+}
+
+function mouseEnterCallback(ev: Event) {
+	startFadeIn(ev.target as HTMLElement | SVGElement);
+}
+function mouseLeaveCallback(ev: Event) {
+	startFadeOut(ev.target as HTMLElement | SVGElement);
+}
+
+function fadeFrame(newTime: number) {
+	curTime = newTime;
+	requestAnimationFrame(fadeFrame);
+	hoverDataMap.forEach((hoverData, elem) => {
+		const timeFraction = (
+			curTime - hoverData.fadeStartTime
+		) / (
+			hoverData.fadeTime
+		);
+		if (timeFraction >= 1) {
+			hoverDataMap.delete(elem);
+		}
+		const fadeFraction = hoverData.timingFunction(
+			hoverData.fadingOut ? (1 - timeFraction) : timeFraction
+		);
+		if (elem == document.documentElement) {
+			document.documentElement.style.setProperty(
+				"--cursor-window-pos-fade-fraction",
+				fadeFraction + ""
+			);
+			document.documentElement.style.setProperty(
+				"--cursor-window-pos-fade-percentage",
+				(fadeFraction * 100) + "%"
+			);
+		} else {
+			elem.style.setProperty("--cursor-pos-fade-fraction", fadeFraction + "");
+			elem.style.setProperty("--cursor-pos-fade-percentage", (fadeFraction * 100) + "%");
+		}
+
+	});
+}
+requestAnimationFrame(fadeFrame);
+
+function listenToAllElements() {
+	hoverClasses.forEach(className => {
+		for (const elem of document.getElementsByClassName(className)) {
+			if (!hoverDataMap.has(elem as HTMLElement | SVGElement)) {
+				startListeningToElement(elem as HTMLElement | SVGElement);
+			}
+		}
+	});
+}
+export function addFollowCursorClass(...classes: string[]) {
+	classes.forEach(className => hoverClasses.add(className));
+	listenToAllElements();
+}
+
+function upateMousePosVars(elem: HTMLElement | SVGElement) {
+	if (elem == document.documentElement) {
+		const fractionX = mouseX / window.innerWidth;
+		const fractionY = mouseY / window.innerHeight;
+		elem.style.setProperty("--cursor-window-pos-x-px", mouseX + "px");
+		elem.style.setProperty("--cursor-window-pos-y-px", mouseY + "px");
+		elem.style.setProperty("--cursor-window-pos-x-fraction", fractionX + "");
+		elem.style.setProperty("--cursor-window-pos-y-fraction", fractionY + "");
+		elem.style.setProperty("--cursor-window-pos-x-percentage", (fractionX * 100) + "%");
+		elem.style.setProperty("--cursor-window-pos-y-percentage", (fractionY * 100) + "%");
+	} else {
+		const elemRect = elem.getBoundingClientRect();
+		const elemMouseX = (mouseX - elemRect.left);
+		const elemMouseY = (mouseY - elemRect.top);
+		const fractionX = elemMouseX / elemRect.width;
+		const fractionY = elemMouseY / elemRect.height;
+		elem.style.setProperty("--cursor-pos-x-px", elemMouseX + "px");
+		elem.style.setProperty("--cursor-pos-y-px", elemMouseY + "px");
+		elem.style.setProperty("--cursor-pos-x-fraction", fractionX + "");
+		elem.style.setProperty("--cursor-pos-y-fraction", fractionY + "");
+		elem.style.setProperty("--cursor-pos-x-percentage", (fractionX * 100) + "%");
+		elem.style.setProperty("--cursor-pos-y-percentage", (fractionY * 100) + "%");
+	}
+}
+
+function listenToDOMChanges() {
+	document.documentElement.addEventListener("mousemove", ev => {
+		mouseX = ev.clientX;
+		mouseY = ev.clientY;
+		listenedElements.forEach(elem => {
+			if (
+				(!hoverDataMap.has(elem) && !elem.matches(":hover"))
+			) {
+				return;
+			}
+			upateMousePosVars(elem);
+		});
+	}, {passive: true});
+	const observer = new MutationObserver((mutations) => {
+		for (let i = 0; i < mutations.length; i += 1) {
+			const mutation = mutations[i]
+			if (!(mutation.target instanceof HTMLElement) && !(mutation.target instanceof SVGElement)) {
+				continue;
+			}
+			switch (mutation.type) {
+				case "attributes": {
+					const mutationTarget = mutation.target;
+					const hasClass = ![...hoverClasses].every(className => !mutationTarget.classList.contains(className));
+					if (listenedElements.has(mutationTarget)) {
+						if (!hasClass) {
+							stopListeningToElement(mutationTarget);
+						}
+					} else {
+						if (hasClass) {
+							startListeningToElement(mutationTarget);
+						}
+					}
+					break;
+				}
+				case "childList": {
+					mutation.addedNodes.forEach(elem => {
+						if (!(elem instanceof HTMLElement) && !(elem instanceof SVGElement)) {
+							return;
+						}
+						if ([...hoverClasses].every(className => !elem.classList.contains(className))) {
+							return;
+						}
+						startListeningToElement(elem);
+					});
+					mutation.removedNodes.forEach(elem => {
+						if (!(elem instanceof HTMLElement) && !(elem instanceof SVGElement)) {
+							return;
+						}
+						if (!listenedElements.has(elem)) {
+							return;
+						}
+						stopListeningToElement(elem);
+					});
+					break;
+				}
+				default:
+			}
+		}
+	});
+	// Doing this in 2 steps because we don't care about changes to the <head>
+	observer.observe(document.body, {subtree: true, childList: true, attributeFilter: ["class"]});
+	observer.observe(document.documentElement, {attributeFilter: ["class"]});
+
+	listenToAllElements();
+}
+if (document.readyState == "loading") {
+	document.addEventListener("DOMContentLoaded", listenToDOMChanges);
+} else {
+	listenToDOMChanges();
+}
